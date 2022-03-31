b92db97d397587feaa352bbca14e74fdc219ac51
Diana <oganauskiene@gmail.com>
2022-03-31 15:00:51 +0300

    kovo 31

---
+++ js/kovo31.js
@@ -0,0 +1,49 @@
+const x = 222222
+console.log(scrollX);
+
+/*
+3. Kintamųjų palyginimas
+Lyginant, jei rezultatas tenkina palyginimo sąlygą, tai į console išvesti žodį “Pomidoras”, o jei sąlyga nėra tenkinama, išvesti sakinį “Bandykite kitą kartą.”.
+
+1. Tarpusavyje palyginti skaičiaus tipo kintamuosius:
+kuris didesnis
+kuris mažesnis
+ar jie lygūs
+ar jie nelygūs
+kuris didesnis arba lygus
+kuris mažesnis arba lygus
+*/
+
+const a = 2;
+const b = 9;
+
+if (a > b) {
+    console.log('Pomidoras');
+} else (a < b) {
+    console.log(Bandykite kitą kartą.);
+}
+
+
+/*
+2. Išvesti teksto tipo kintamųjų ilgius
+Tarpusavyje palyginti teksto tipo kintamųjų ilgius:
+kuris didesnis
+kuris mažesnis
+ar jie lygūs
+ar jie nelygūs
+kuris didesnis arba lygus
+kuris mažesnis arba lygus
+*/
+
+/*
+3. Išvesti sąrašo tipo kintamųjų ilgius
+Tarpusavyje palyginti sąrašo tipo kintamųjų ilgius:
+kuris didesnis
+kuris mažesnis
+ar jie lygūs
+ar jie nelygūs
+kuris didesnis arba lygus
+kuris mažesnis arba lygus
+*/
+
+
